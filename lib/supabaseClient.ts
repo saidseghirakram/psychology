c08bccf8cf79eba060cfd962030c15129e107b95
S-fetah <fettahsafi.6@gmail.com>
2025-05-28 10:19:50 +0100

    additional fixes

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -2,10 +2,12 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const email = process.env.SUPABASE_PWS!;
+const password = process.env.SUPABASE_EMAIL!;
 export async function SignIn() {
   const { data, error } = await supabase.auth.signInWithPassword({
-    email: "fettah@gmail.com",
-    password: "123",
+    email: email,
+    password: password,
   });
   if (error) return console.error(error);
   return console.log("signed in successfully ", data);
